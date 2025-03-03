15c7b41addd36d74c9b058e42f265ad486073963
RichardBray <richardbray89@gmail.com>
2025-03-03 10:09:01 +0000

    feat: added opentelemetry hook

--- server/main.py
+++ server/main.py
@@ -3,8 +3,29 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from openfeature import api
 from openfeature.contrib.provider.flagd import FlagdProvider
+from openfeature.contrib.hook.opentelemetry import TracingHook
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 app = FastAPI()
+
+def setup_telemetry():
+    otlp_exporter = OTLPSpanExporter(
+        endpoint="http://localhost:4318/v1/traces",
+    )
+
+    trace_provider = TracerProvider()
+    processor = BatchSpanProcessor(otlp_exporter)
+    trace_provider.add_span_processor(processor)
+    trace.set_tracer_provider(trace_provider)
+
+setup_telemetry()
+# Instrument FastAPI after setup
+FastAPIInstrumentor.instrument_app(app)
 
 api.set_provider(FlagdProvider())
 
@@ -17,7 +38,10 @@ app.add_middleware(
 )
 
 client = api.get_client()
+client.add_hooks([TracingHook()])
 FLAG_STR="welcome-message"
+
+
 
 @app.get("/api/buy-now")
 def root():
