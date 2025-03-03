17162f541e5fe09dcba1e5bb2e986d473902f2e4
RichardBray <richardbray89@gmail.com>
2025-03-03 10:12:28 +0000

    feat: trim down feature flag code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,7 @@
-import { useFlag } from "@openfeature/react-sdk";
-
 import "./App.css";
 import { useState } from "react";
-const FLAG_STR="welcome-message";
 
 function App() {
-  const { value: showNewMessage } = useFlag(FLAG_STR, true);
   const [apiMessage, setApiMessage] = useState<string>("");
 
   async function handleBuyNow() {
@@ -22,19 +18,16 @@ function App() {
     <>
       <h1>My Cool Store</h1>
       <div className="card">
-        {showNewMessage ? (
-          <>
-            <h2>My cool item</h2>
-            <p>This is the best item ever</p>
-            <button type="button" onClick={handleBuyNow}>
-              Buy Now
-            </button>
-            {apiMessage && (
-              <p role="status" aria-live="polite">
-                {apiMessage}
-              </p>)}
-            </>
-          ) : <p>Under construction</p>}
+        <h2>My cool item</h2>
+        <p>This is the best item ever</p>
+        <button type="button" onClick={handleBuyNow}>
+          Buy Now
+        </button>
+        {apiMessage && (
+          <p role="status" aria-live="polite">
+            {apiMessage}
+          </p>
+        )}
       </div>
     </>
   );
--- server/main.py
+++ server/main.py
@@ -1,59 +1,20 @@
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from openfeature import api
-from openfeature.contrib.provider.flagd import FlagdProvider
-from openfeature.contrib.hook.opentelemetry import TracingHook
-
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 app = FastAPI()
 
-def setup_telemetry():
-    otlp_exporter = OTLPSpanExporter(
-        endpoint="http://localhost:4318/v1/traces",
-    )
-
-    trace_provider = TracerProvider()
-    processor = BatchSpanProcessor(otlp_exporter)
-    trace_provider.add_span_processor(processor)
-    trace.set_tracer_provider(trace_provider)
-
-setup_telemetry()
-# Instrument FastAPI after setup
-FastAPIInstrumentor.instrument_app(app)
-
-api.set_provider(FlagdProvider())
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-client = api.get_client()
-client.add_hooks([TracingHook()])
-FLAG_STR="welcome-message"
-
-
-
 @app.get("/api/buy-now")
 def root():
-    show_welcome_message = client.get_boolean_value(FLAG_STR, True)
-
-    if show_welcome_message:
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content={"message": "Payment successful"},
-        )
-    else:
-        return JSONResponse(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            content={"message": "Not authorized"},
-        )
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"message": "Payment successful"},
+    )
